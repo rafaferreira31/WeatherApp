b2ae368194cee9c1a4813e6906050d9a02c43c26
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-12-04 12:15:33 +0000

    Add project files.

---
+++ WeatherApp/Services/ApiService.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+        public static readonly string _baseUrl = "https://nmbd2wm8-7066.uks1.devtunnels.ms/";
+        private readonly ILogger<ApiService> _logger;
+        JsonSerializerOptions _serializerOptions;
+
+
+        public ApiService(HttpClient httpClient,
+                          ILogger<ApiService> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+
+        public async Task<ApiResponse<bool>> RegistrarUsuario(string nome, string email,
+                                                     string telefone, string password)
+        {
+            try
+            {
+                var register = new Register()
+                {
+                    Nome = nome,
+                    Email = email,
+                    Telefone = telefone,
+                    Senha = password
+                };
+
+                var json = JsonSerializer.Serialize(register, _serializerOptions);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await PostRequest("api/Usuarios/Register", content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    _logger.LogError($"Error sending HTTP request: {response.StatusCode}");
+                    return new ApiResponse<bool>
+                    {
+                        ErrorMessage = $"Error sending HTTP request: {response.StatusCode}"
+                    };
+                }
+
+                return new ApiResponse<bool> { Data = true };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Erro registering this user: {ex.Message}");
+                return new ApiResponse<bool> { ErrorMessage = ex.Message };
+            }
+        }
+
+
+
+        public async Task<ApiResponse<bool>> Login(string email, string password)
+        {
+            try
+            {
+                var login = new Login()
+                {
+                    Email = email,
+                    Senha = password
+                };
+
+                var json = JsonSerializer.Serialize(login, _serializerOptions);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await PostRequest("api/Usuarios/Login", content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    _logger.LogError($"Error sending HTTP request: {response.StatusCode}");
+                    return new ApiResponse<bool>
+                    {
+                        ErrorMessage = $"Error sending HTTP request: {response.StatusCode}"
+                    };
+                }
+
+                var jsonResult = await response.Content.ReadAsStringAsync();
+                var result = JsonSerializer.Deserialize<Token>(jsonResult, _serializerOptions);
+
+                Preferences.Set("accesstoken", result!.AccessToken);
+                Preferences.Set("usuarioid", (int)result.UsuarioId!);
+                Preferences.Set("usuarionome", result.UsuarioNome);
+
+                return new ApiResponse<bool> { Data = true };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Login Error: {ex.Message}");
+                return new ApiResponse<bool> { ErrorMessage = ex.Message };
+            }
+        }
+
+        private async Task<HttpResponseMessage> PostRequest(string uri, HttpContent content)
+        {
+            var enderecoUrl = _baseUrl + uri;
+            try
+            {
+                var result = await _httpClient.PostAsync(enderecoUrl, content);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error sending POST request for {uri}: {ex.Message}");
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
